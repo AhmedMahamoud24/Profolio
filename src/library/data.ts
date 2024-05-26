46c37c73554705a2a45266cf77fbea7015c355e1
AhmedMahamoud244 <120144072+AhmedMahamoud244@users.noreply.github.com>
2024-05-26 01:30:40 -0400

    Initial commit

---
+++ src/library/data.ts
@@ -0,0 +1,71 @@
+// 📦 Projects list: Customize with your own project details.
+export const projects = [
+  {
+    title: 'XO Rumble', // Enter project title.
+    githubUrl: '', // Enter GitHub URL.
+    externalUrl: '', // Enter live site URL.
+    languages: ['Zustand', 'React', 'TypeScript'], // Specify technologies used.
+  },
+  {
+    title: 'Bogetta',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['Next.js', 'TypeScript', 'Prisma', 'Stripe'],
+  },
+  {
+    title: 'Bitcoin Game',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['E2E', 'React', 'TypeScript', 'Firebase'],
+  },
+  {
+    title: 'Chat App',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'Firebase'],
+  },
+  {
+    title: 'Netflix Clone',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'Firebase'],
+  },
+  {
+    title: 'Instagram Clone',
+    githubUrl: '',
+    externalUrl: '',
+    languages: ['React', 'TypeScript', 'CSS', 'Firebase'],
+  },
+]
+
+export const socialLinks = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/AhmedMahamoud24', 
+    icon: 'Github',
+  },
+  {
+    name: 'Instagram',
+    url: '',
+    icon: 'Instagram',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/ahmed-m-85a539165/', 
+    icon: 'LinkedIn',
+  },
+  {
+    name: 'Blog',
+    url: '', 
+    icon: 'Blog',
+  },
+]
+
+export const navbarData = {
+  navigator: [
+    { name: 'Home' },
+    { name: 'About' },
+    { name: 'Projects' },
+    { name: 'Contact' },
+  ],
+}
